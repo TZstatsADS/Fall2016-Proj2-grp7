8664bcc4012353fc8050073b65b7066da508da97
Jiayiwang7 <jiayiwang7@yahoo.com>
2016-10-09 16:41:00 -0400

    updated

--- app/ui.R
+++ app/ui.R
@@ -10,9 +10,12 @@ dashboardPage(
                   titleWidth = 300
                   ),
   
+  
+  
   dashboardSidebar(
- 
+    
     width = 300,
+    
     ## Customize Header
     tags$head(tags$style(HTML('.main-header .logo {
                               font-family: "Avenir",Avenir, "Avenir", serif;
@@ -23,7 +26,7 @@ dashboardPage(
     
     ## Get user location
     tags$script('
-      $(document).ready(function () {
+                $(document).ready(function () {
                 navigator.geolocation.getCurrentPosition(onSuccess, onError);
                 
                 function onError (err) {
@@ -41,30 +44,46 @@ dashboardPage(
                 }
                 });
                 '),
-    tags$br(),
-    div(strong('Starting Position', style = 'font-size:10.7pt'), style = 'line-height:200%;padding-left:11px'),
-    sidebarSearchForm(textId = 'userlocation', buttonId = 'searchButton', 
-                      icon = shiny::icon('location-arrow'), label = 'e.g.: Columbia Univeristy'),
-    sliderInput(inputId = 'distance', label = 'Distance (Mile)', value = 3, min = 0, max = 15, step = 0.1),
     
-    tags$hr(),
-    radioButtons('airquality', 'Air Quality',
-                 c('Very Important' = 5, 'Important' = 4, 'Fair' = 3, "Doesn't Matter" = 1), 'Important'),
-    radioButtons('safety', 'Safety', 
-                 c('Very Important' = 5, 'Important' = 4, 'Fair' = 3, "Doesn't Matter" = 1), 'Fair'),
-
-    checkboxInput('park', 'Park & Garden', FALSE),
-    checkboxInput('river', 'River & Pool', FALSE),
-    checkboxInput('track', 'Running Track', FALSE),
-    checkboxInput('drink', 'Drinking Fountain', FALSE),
-    checkboxInput('dog', 'Dog Runs & Off-Leash Area', FALSE),
-    tags$br(),
-    div(actionButton('submit', 'Run', icon = shiny::icon('blind')), style = 'padding-left: 11px')
+    sidebarMenu(id = 'sidebarmenu', 
+                menuItem('Route Planner', tabName = 'routeplan', icon = shiny::icon('street-view'), 
+                         div(strong('Starting Position', style = 'font-size:10.7pt'), style = 'line-height:200%;padding-left:11px'),
+                         sidebarSearchForm(textId = 'userlocation', buttonId = 'searchButton', 
+                                           icon = shiny::icon('location-arrow'), label = 'e.g.: Columbia Univeristy'),
+                         sliderInput(inputId = 'distance', label = 'Distance (Mile)', value = 3, min = 0, max = 15, step = 0.1),
+                         
+                         tags$hr(),
+                         radioButtons('airquality', 'Air Quality',
+                                      c('Very Important' = 5, 'Important' = 4, 'Fair' = 3, "Doesn't Matter" = 1), 'Important'),
+                         radioButtons('safety', 'Safety', 
+                                      c('Very Important' = 5, 'Important' = 4, 'Fair' = 3, "Doesn't Matter" = 1), 'Fair'),
+                         
+                         checkboxInput('park', 'Park & Garden', FALSE),
+                         checkboxInput('river', 'River & Pool', FALSE),
+                         checkboxInput('track', 'Running Track', FALSE),
+                         checkboxInput('drink', 'Drinking Fountain', FALSE),
+                         checkboxInput('dog', 'Dog Runs & Off-Leash Area', FALSE),
+                         tags$br(),
+                         div(actionButton('submit', 'Run', icon = shiny::icon('blind')), style = 'padding-left: 11px')
+                ),
+                menuItem('Running Map Overview', tabName = 'runmap', icon = shiny::icon('map-pin'))
+                )
+ 
+    
+  
     
   ),
   
   dashboardBody(
-    leafletOutput('map', width = '100%', height = 1000)
+    
+    tabItem(tabName = 'routeplan',
+            leafletOutput('map', width = '100%', height = 1000)
+    ),
+    
+    tabItem(tabName = 'runmap',
+            div(strong('Starting Position', style = 'font-size:10.7pt'), style = 'line-height:200%;padding-left:11px')
+            )
+    
     
   )
 )
