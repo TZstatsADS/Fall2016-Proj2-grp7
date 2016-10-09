432f0e50922984f02709493e39b28fc3e51b67a2
ClayCai <caikezhi@yeah.net>
2016-10-09 19:54:03 -0400

    Revised Version

--- lib/Meander Function.R
+++ lib/Meander Function.R
@@ -6,7 +6,7 @@ Meander= function(start_lon,start_lat,end_lon,end_lat,running_distance_in_mile,N
   library(maps)
   library(osrm)
   library(geosphere)
-# Need Nodes: 1. Longi; 2. Lati;
+#  1. Longi; 2. Lati;
   node_info=matrix(0,nrow=length(Scores)+2,ncol=3)
 
   node_info[1:length(Scores),]=c(Nodes[,1],Nodes[,2],Scores)
@@ -14,7 +14,11 @@ Meander= function(start_lon,start_lat,end_lon,end_lat,running_distance_in_mile,N
   node_info[num-1,]=c(start_lon,start_lat,0)
   node_info[num,]=c(end_lon,end_lat,0)
   start_node=num-1;
-  end_node=num;
+  if(node_info[num,1]==-1) 
+    {
+      end_node=start_node;
+      num=num-1
+  }
 ############## Prep ###################    
   distance=distm(node_info[,1:2])*1.25
   close_neib=t(apply(distance,2,order))
@@ -24,13 +28,16 @@ Meander= function(start_lon,start_lat,end_lon,end_lat,running_distance_in_mile,N
   bestscore=-9999
   finaldist=0
 ################# Search ##################  
+  a=Sys.time()
   route=array(0,dim=num)
   g=environment()
   findroute=function(x,d,tt) # x: current position; d: distance remains; tt: how many nodes visited, does not include x
   {
+   b=Sys.time()
+   if((b-a)<10){                               # Change stop timer here
     if(x!=end_node) g$nodes_passed[x]=TRUE
     g$route[tt+1]=x
-    if (x==end_node)
+    if ((x==end_node) & (tt>0))
     {
       g$score=sum(node_info[route,3])
       if(g$score>g$bestscore)
@@ -55,31 +62,56 @@ Meander= function(start_lon,start_lat,end_lon,end_lat,running_distance_in_mile,N
       if(distance[x,end_node]<=d) findroute(end_node,d-distance[x,end_node],tt+1)
     }
     g$nodes_passed[x]=FALSE
+   }
   }
   
- 
-  a=Sys.time()
   findroute(start_node,running_distance_in_mile*1609,0)
-  b=Sys.time()
-  b-a
+  
   stops=bestroute[length(bestroute)]
   bestroute=bestroute[1:stops]  
-
+  print(bestroute)
+  print(bestscore)
+  
   route_in_osm=matrix(node_info[start_node,1:2],ncol=2)
   for (i in 2:stops)
   {
     Akke=osrmRoute(c(1,node_info[bestroute[i-1],1],node_info[bestroute[i-1],2]),c(2,node_info[bestroute[i],1],node_info[bestroute[i],2]))
-    print(Akke)
     route_in_osm=rbind(route_in_osm,as.matrix(Akke),node_info[bestroute[i],1:2])
     }
   return(list(bestroute,route_in_osm))
 }
 
-cai=Meander(-73.97396,40.78870,-73.96862,40.78819,5,randomnode[,1:2],randomnode[,3])
 
-x=mean(randomnode[start_node,1],randomnode[end_node,1])
-y=mean(randomnode[start_node,2],randomnode[end_node,2])
+################################ Generating Random Nodes ##########################
+trials=400 
+border=matrix(c(40.7,40.690,-74.043,-73.983,40.877,40.808,-73.930,-73.916),nrow=2)
+b=matrix(0,nrow=2,ncol=2)
+for(i in 1:2)
+{
+  b[1,i]=(border[i,3]-border[i,1])/(border[i,4]-border[i,2]) # slope
+  b[2,i]=border[i,1]-border[i,2]*b[1,i]                      # intercept
+}
+randomnode=matrix(nrow=trials,ncol=3)
+randomnode[,1]=runif(trials,-74.025,-73.906)
+randomnode[,2]=runif(trials,40.701,40.877)
+for(i in 1:trials)
+{
+  if((randomnode[i,1]*b[1,1]+b[2,1]) < randomnode[i,2]) randomnode[i,]=NA
+  else if((randomnode[i,1]*b[1,2]+b[2,2]) > randomnode[i,2]) randomnode[i,]=NA
+}
+for (i in trials:1)
+  if (is.na(randomnode[i,1])) randomnode=randomnode[-i,]
+num=dim(randomnode)[1]
+randomnode[,3]=runif(num,0,100)
+
+
+#######################################################################
+cai=Meander(-73.97396,40.78870,-1,-1,5,randomnode[,1:2],randomnode[,3])
+
+
+#x=mean(randomnode[start_node,1],randomnode[end_node,1])
+#y=mean(randomnode[start_node,2],randomnode[end_node,2])
 m <- leaflet() %>% setView(lng = x, lat =y, zoom =13)
 m= m %>%addTiles() 
 m= m %>% addCircles(lng=randomnode[,1],lat=randomnode[,2],radius=100,col="red")
-m %>% addPolylines (lng=cai[,1],lat=cai[,2])
+m %>% addPolylines (lng=cai[[2]][,1],lat=cai[[2]][,2])
