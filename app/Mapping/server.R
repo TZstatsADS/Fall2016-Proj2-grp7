05557ca453d54680a0c89c65054e88d492f55d3c
Shujin Cao (Jackie) <sc3977@columbia.edu>
2016-10-11 13:49:46 -0400

    change

--- app/Mapping/server.R
+++ app/Mapping/server.R
@@ -7,116 +7,184 @@ library(maps)
 library(mapdata)
 
 
-
+run <- read.csv("test.csv")
 
 
 shinyServer(function(input,output){
   
-
-     #output$map <- renderLeaflet({
   
-        #Create icon for running spots 
-       
-        runIcon <-
-          makeIcon(
-          iconUrl = "runner icon.png",
-          iconWidth = 40, iconHeight = 40
-        )
-        dogIcon <-
-          makeIcon(
-          iconUrl = "dog icon.png",
-          iconWidth = 30, iconHeight = 30
-        )
-
-
-#set preference for runing environment
-         #run = read.csv("test.csv")
-        preference <-  reactive({
-          rrr <- run
-          if(input$park == TRUE){
-            rrr = filter(rrr, Park == 1)
-          }
-          if(input$riverside == TRUE){
-            rrr = filter(rrr, Riverside == 1)
-          }
-          if(input$dog == TRUE){
-            rrr = filter(rrr, Dog == "1")
-          }
-          if(input$drinkingfountain ==TRUE){
-            rrr = filter(rrr, DrinkingFountain == "1")
-          }
-          
-          if(input$safety == "Very Important to me!"){
-            rrr = filter(rrr, Safety == "5")
-          }
-          if(input$safety == "Let it be okay"){
-            rrr = filter(rrr, Safety >= 3) 
-          }
-          if(input$safety == "Not Care at all"){
-            rrr = rrr
-          }
-          if(input$airquality == "Very Important to me!"){
-            rrr = filter(rrr, Airquality == "5")
-          }
-          if(input$airquality == "Let it be okay"){
-            rrr = filter(rrr, Airquality >= 3)
-          }
-          if(input$airquality == "Not Care at all"){
-            rrr = rrr
-          }
-          return(rrr)
-          
-        })
+  
+  #output$map <- renderLeaflet({
+  
+  #Create icon for running spots 
+  
+  
+  parkIcon <-
+    makeIcon(
+      iconUrl = "park icon.png",
+      iconWidth = 40, iconHeight = 40
+    )
+  riverIcon <-
+    makeIcon(
+      iconUrl = "river icon.png",
+      iconWidth = 40, iconHeight = 40
+    ) 
+  waterIcon <-
+    makeIcon(
+      iconUrl = "water icon.png",
+      iconWidth = 40, iconHeight = 40
+    )
+  
+  dogIcon <-
+    makeIcon(
+      iconUrl = "dog icon.png",
+      iconWidth = 30, iconHeight = 30
+    )
+  
+  
+  #set preference for runing environment
+  #run = read.csv("test.csv")
+  preference <-  reactive({
+    rrr <- run
+    if(input$park == TRUE){
+      rrr = filter(rrr, Park == 1)
+    }
+    else if (input$park ==FALSE){
+      rrr = filter(rrr,Park == 3)
+    }
+    
+    # if(input$safety == "Very Important to me!"){
+    #   rrr = filter(rrr, Safety == "5")
+    # }
+    # if(input$safety == "Let it be okay"){
+    #   rrr = filter(rrr, Safety >= 3) 
+    # }
+    # if(input$safety == "Not Care at all"){
+    #   rrr = rrr
+    # }
+    # if(input$airquality == "Very Important to me!"){
+    #   rrr = filter(rrr, Airquality == "5")
+    # }
+    # if(input$airquality == "Let it be okay"){
+    #   rrr = filter(rrr, Airquality >= 3)
+    # }
+    # if(input$airquality == "Not Care at all"){
+    #   rrr = rrr
+    # }
+    
+    return(rrr)
+    
+  })
+  
+  
+  preference1 <- reactive({
+    rrr <- run
+    if(input$riverside == TRUE){
+      rrr = filter(rrr, Riverside == "1")
+    }
+    else rrr = filter(rrr, Riverside == "3")
+    return(rrr)
+  })
+  
+  
+  preference2 <- reactive({
+    rrr <- run
+    if(input$dog == TRUE){
+      rrr = filter(rrr, Dog == "1")
+    }
+    else rrr = filter(rrr, Dog == "3")
+    return(rrr)
+  })
+  
+  preference3 <- reactive({
+    rrr <- run
+    if(input$drinkingfountain ==TRUE){
+      rrr = filter(rrr, DrinkingFountain == "1")
+    }
+    else rrr = filter(rrr, DrinkingFountain == "3")
+    return(rrr)
+  })   
+  
+  preference4 <- reactive({
+    rrr <- run
+    if(input$drinkingfountain ==FALSE){
+      rrr = filter(rrr, DrinkingFountain == "1")
+    }
+    else rrr = filter(rrr, DrinkingFountain == "3")
+    return(rrr)
+  }) 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  #add markers for running spots
+  
+  output$map <- renderLeaflet({
+    leaflet()%>% 
+      # addTiles(urlTemplate = "https://api.mapbox.com/styles/v1/jackiecao/ciu0jcgy800ah2ipgpsw5usmi/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamFja2llY2FvIiwiYSI6ImNpdTBqYXhmcjAxZ24ycGp3ZWZ1bTJoZ3QifQ.VytIrn5ZxVjtZjM15JPA9Q",
+      #          attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+      # )%>%
+      addProviderTiles("Stamen.Watercolor") %>% 
+      setView(lng = -73.97, lat = 40.75, zoom = 13)  
+    
+  })
+  observe({
+    proxy =
+      leafletProxy("map",data = preference())
+    proxy %>%
       
-        
-         preference1 <- reactive({
-           rrr <- run
-           if(input$drinkingfountain ==TRUE){
-             rrr = filter(rrr, DrinkingFountain == "1")
-           }
-           else rrr = filter(rrr, DrinkingFountain == "3")
-           return(rrr)
-         })
-         
-         preference2 <- reactive({
-           rrr <- run
-           if(input$dog == TRUE){
-             rrr = filter(rrr, Dog == "1")
-           }
-           else rrr = filter(rrr, Dog == "3")
-           return(rrr)
-         })
-     
-
-
-
-
- 
- 
-#add markers for running spots
-
-         output$map <- renderLeaflet({
-           leaflet()%>% 
-             # addTiles(urlTemplate = "https://api.mapbox.com/styles/v1/jackiecao/ciu0jcgy800ah2ipgpsw5usmi/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamFja2llY2FvIiwiYSI6ImNpdTBqYXhmcjAxZ24ycGp3ZWZ1bTJoZ3QifQ.VytIrn5ZxVjtZjM15JPA9Q",
-             #          attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
-             # )%>%
-             addProviderTiles("Stamen.Watercolor")%>%
-             #setView(lng = -73.97, lat = 40.75, zoom = 13) %>% 
-             #addMarkers(data = run, ~long, ~lat,icon = runIcon)   
-             addMarkers(data = preference(), ~long, ~lat, icon = runIcon
-                        ,popup = paste("*Address:",preference()$address,"<br>")
-             ) %>% 
-             addMarkers(data = preference(), ~long, ~lat, icon = runIcon
-                        ,popup = paste("*Address:",preference()$address,"<br>")
-             ) %>% 
-             addMarkers(data = preference2(), ~long, ~lat,icon = dogIcon
-                        ,popup = paste("*Address:",preference()$address,"<br>")
-             )
-         })
-           
-
+      addMarkers( #data = preference(),
+        ~long, ~lat, icon = parkIcon
+        ,popup = paste("*Address:",preference()$address,"<br>"), group = "a"
+      ) 
+    
+  })
+  
+  observe({
+    proxy=
+      leafletProxy("map",data = preference1())
+    proxy %>% 
+      # clearMarkers() %>%
+      addMarkers(#data = preference1(), 
+        ~long, ~lat, icon = riverIcon
+        ,popup = paste("*Address:",preference1()$address,"<br>")
+      )
+  })
+  
+  observe({
+    proxy=
+      leafletProxy("map",data = preference2())
+    proxy %>%
+      addMarkers(#data = preference2(),
+        ~long, ~lat,icon = dogIcon
+        ,popup = paste("*Address:",preference2()$address,"<br>")
+      )
+    
+  })
+  
+  
+  observe({
+    proxy =
+      leafletProxy("map",data = preference3()) 
+    proxy %>% 
+      # clearMarkers() %>%
+      addMarkers(#data = preference2(),
+        ~long, ~lat, icon = waterIcon
+        ,popup = paste("*Address:",preference3()$address,"<br>", layerId = "w")
+      )
+    
+  })
+  
+  
+  
 })
 
 
-   
+
 
--- app/Mapping/ui.R
+++ app/Mapping/ui.R
@@ -15,58 +15,57 @@ AirQuality <- c("Very Important to me!",
                 "Let it be okay",
                 "Not Care at all" 
 )
- 
-shinyUI(
-        dashboardPage(skin = "yellow", 
-                      dashboardHeader(title = "Running Manhattan"),
-                      dashboardSidebar(tags$head(tags$style(HTML('.main-header .logo {font-family: "Britannic Bold",Britannic Bold, 
-                                                                 "Britannic Bold", serif;font-weight: bold;font-size: 22px;}'
-                                                            )
-                                                 )
-                                       ),
-                  
-                          sidebarMenu(
-                          menuItem("Running Map", tabName = "mapping", icon = icon("map")
-                                   ,checkboxInput("park", "GO Park", width = "90%", value = TRUE)
-                                   ,checkboxInput("riverside","Go Riverside", width = "90%")
-                                   ,checkboxInput("dog", "Dog Friendly",width = "90%", value = TRUE)
-                                   ,checkboxInput("drinkingfountain","Drinking Fountain Accessible", width = "90%")
-                                   ,selectInput("safety","Safety",SafetyLevel, selected = "Let it be okay")
-                                   ,selectInput("airquality", "Air Quality", AirQuality)),
-                          menuItem("Running Routes Planner", tabName = "routes", icon = icon("map-signs"))
-                          )
-                      ),
-                      dashboardBody(#div(class = "outer",
-                                          #tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
-                                          #tags$head(includeCSS("styles.css"))
-                                         
-                                    #),
-                                     tags$head(tags$link(rel = "icon", type = "image/png", href = "favicon.png"),
-                                     tags$title("Popular Running Spots")),
-                                     
-                                             tabItem(tabName = "mapping",
-                                                     leafletOutput("map",height = 800)
-                                               
-                                              )
-                                    
-                      ) 
-          )
-        
-)
-          
 
-         
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
+shinyUI(
+  dashboardPage(skin = "yellow", 
+                dashboardHeader(title = "Running Manhattan"),
+                dashboardSidebar(tags$head(tags$style(HTML('.main-header .logo {font-family: "Britannic Bold",Britannic Bold, 
+                                                           "Britannic Bold", serif;font-weight: bold;font-size: 22px;}'
+                )
+                )
+                ),
+                
+                sidebarMenu(
+                  menuItem("Running Map", tabName = "mapping", icon = icon("map")
+                           ,checkboxInput("park", "GO Park", width = "90%", value = TRUE)
+                           ,checkboxInput("riverside","Go Riverside", width = "90%")
+                           ,checkboxInput("dog", "Dog Friendly",width = "90%", value = TRUE)
+                           ,checkboxInput("drinkingfountain","Drinking Fountain Accessible", width = "90%")
+                           ,selectInput("safety","Safety",SafetyLevel, selected = "Let it be okay")
+                           ,selectInput("airquality", "Air Quality", AirQuality)),
+                  menuItem("Running Routes Planner", tabName = "routes", icon = icon("map-signs"))
+                )
+                ),
+                dashboardBody(#div(class = "outer",
+                  #tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
+                  #tags$head(includeCSS("styles.css"))
+                  
+                  #),
+                  tags$head(tags$link(rel = "icon", type = "image/png", href = "favicon.png"),
+                            tags$title("Popular Running Spots")),
+                  
+                  tabItem(tabName = "mapping",
+                          leafletOutput("map",height = 800)
+                          
+                  )
+                  
+                ) 
+  )
+  
+  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
