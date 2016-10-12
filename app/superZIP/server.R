ce18b433696539041d67cab1ce1acc96fbd43ade
valenese <rl2633@tc.columbia.edu>
2016-10-12 12:58:00 -0400

    Add files via upload

--- app/superZIP/server.R
+++ app/superZIP/server.R
@@ -12,6 +12,7 @@ library(leaflet)
 library(maps)
 library(osrm)
 library(geosphere)
+library(shiny)
 
 Meander <- function(start_lon,start_lat,end_lon,end_lat,running_distance_in_mile,Nodes,Scores)
 {
@@ -185,7 +186,6 @@ shinyServer(function(input, output){
 
   ########################Second Menu Running Map Overview######################
   #Create icon for running spots 
-  
   parkIcon <-
     makeIcon(
       iconUrl = "park icon.png",
@@ -210,80 +210,125 @@ shinyServer(function(input, output){
   
   
   #set preference for runing environment
-  #run = read.csv("test.csv")
+  
+  
   preference <-  reactive({
     rrr <- run
-    if(input$park == TRUE){
+    if(input$park2 == TRUE){
       rrr = filter(rrr, Park == 1)
     }
-    else if (input$park ==FALSE){
+    if(input$park2 == FALSE){
       rrr = filter(rrr,Park == 3)
     }
-    
-    # if(input$safety == "Very Important to me!"){
-    #   rrr = filter(rrr, Safety == "5")
-    # }
-    # if(input$safety == "Let it be okay"){
-    #   rrr = filter(rrr, Safety >= 3) 
-    # }
-    # if(input$safety == "Not Care at all"){
-    #   rrr = rrr
-    # }
-    # if(input$airquality == "Very Important to me!"){
-    #   rrr = filter(rrr, Airquality == "5")
-    # }
-    # if(input$airquality == "Let it be okay"){
-    #   rrr = filter(rrr, Airquality >= 3)
-    # }
-    # if(input$airquality == "Not Care at all"){
-    #   rrr = rrr
-    # }
-    
+    if(input$safety2 == 5){
+      rrr = filter(rrr, Safety == 5)
+    }
+    if(input$safety2 == 3){
+      rrr = filter(rrr, Safety >= 3)
+    }
+    if(input$safety2 == 0){
+      rrr = filter(rrr, Safety >0)
+    }
+    if(input$airquality2 == 5){
+      rrr = filter(rrr, Airquality == 5)
+    }
+    if(input$airquality2 == 3){
+      rrr = filter(rrr, Airquality >= 3)
+    }
+    if(input$airquality2 == 0){
+      rrr = filter(rrr, Airquality >0)
+    }
     return(rrr)
-    
   })
   
-  # preference4 <-  reactive({
-  #   rrr <- run
-  #   if(input$park == FALSE){
-  #     rrr = filter(rrr, Park == 1)
-  #   }
-  #   else if (input$park ==TRUE){
-  #     rrr = filter(rrr,Park == 3)
-  #     return(rrr)
-  #   }
-  #   })
   
   preference1 <- reactive({
     rrr <- run
     if(input$riverside == TRUE){
-      rrr = filter(rrr, Riverside == "1")
+      rrr = filter(rrr, Riverside == 1)
     }
-    else rrr = filter(rrr, Riverside == "3")
+    if(input$riverside == FALSE){
+      rrr = filter(rrr, Riverside == 3)
+    }
+    if(input$safety2 == 5){
+      rrr = filter(rrr, Safety == 5)
+    }
+    if(input$safety2 == 3){
+      rrr = filter(rrr, Safety >= 3)
+    }
+    if(input$safety2 == 0){
+      rrr = filter(rrr, Safety >0)
+    }
+    if(input$airquality2 == 5){
+      rrr = filter(rrr, Airquality == 5)
+    }
+    if(input$airquality2 == 3){
+      rrr = filter(rrr, Airquality >= 3)
+    }
+    if(input$airquality2 == 0){
+      rrr = filter(rrr, Airquality > 0)
+    }
     return(rrr)
   })
   
   
   preference2 <- reactive({
     rrr <- run
-    if(input$dog == TRUE){
-      rrr = filter(rrr, Dog == "1")
+    if(input$dog2 == TRUE){
+      rrr = filter(rrr, Dog == 1)
     }
-    else rrr = filter(rrr, Dog == "3")
+    if(input$dog2 == FALSE){
+      rrr = filter(rrr, Dog == 3)
+    }
+    # if(input$safety2 == 5){
+    #   rrr = filter(rrr, Safety == 5)
+    # }
+    # if(input$safety2 == 3){
+    #   rrr = filter(rrr, Safety >= 3)
+    # }
+    # if(input$safety2 == 0){
+    #   rrr = filter(rrr, Safety >0)
+    # }
+    # if(input$airquality2 == 5){
+    #   rrr = filter(rrr, Airquality == 5)
+    # }
+    # if(input$airquality2 == 3){
+    #   rrr = filter(rrr, Airquality >= 3)
+    # }
+    # if(input$airquality2 == 0){
+    #   rrr = filter(rrr, Airquality > 0 )
+    # }
     return(rrr)
   })
   
   preference3 <- reactive({
     rrr <- run
     if(input$drinkingfountain ==TRUE){
-      rrr = filter(rrr, DrinkingFountain == "1")
+      rrr = filter(rrr, DrinkingFountains == 1)
     }
-    else rrr = filter(rrr, DrinkingFountain == "3")
+    if(input$drinkingfountain ==FALSE){
+      rrr = filter(rrr, DrinkingFountains == 3)
+    } 
+    # if(input$safety2 == 5){
+    #   rrr = filter(rrr, Safety == 5)
+    # }
+    # if(input$safety2 == 3){
+    #   rrr = filter(rrr, Safety >= 3)
+    # }
+    # if(input$safety2 == 0){
+    #   rrr = filter(rrr, Safety >0)
+    # }
+    # if(input$airquality2 == 5){
+    #   rrr = filter(rrr, Airquality == 5)
+    # }
+    # if(input$airquality2 == 3){
+    #   rrr = filter(rrr, Airquality >= 3)
+    # }
+    # if(input$airquality2 == 0){
+    #   rrr = filter(rrr, Airquality > 0 )
+    # }
     return(rrr)
-  })   
-  
-  
-  
+  })
   
   
   #add markers for running spots
@@ -298,77 +343,72 @@ shinyServer(function(input, output){
     
   })
   
-  # observe({
-  #   proxy =
-  #     leafletProxy("map",session)
-  #   if(!is.null(preference)){
-  #   proxy %>%
-  #     
-  #     addMarkers( data = preference(),
-  #       ~long, ~lat, icon = parkIcon
-  #       ,popup = paste("*Address:",preference()$address,"<br>", layerId = "www")
-  #     )
-  #   }
-  #   if(is.null(preference)){proxy %>%
-  #     addMarkers( data = preference(),
-  #                 ~long, ~lat, icon = parkIcon
-  #                 ,popup = paste("*Address:",preference()$address,"<br>", layerId = "www")
-  #     ) %>%
-  #     removerMarkerCluster(layerId = "www")
-  #   }
-  # })
   
   observe({
     proxy =
       leafletProxy("map2")
     proxy %>%
-      addMarkers( data = preference(),
-                  ~long, ~lat, icon = parkIcon
-                  ,popup = paste("*Address:",preference()$address,"<br>", group = "parkers")
-      )
+      addMarkers(data = preference(), 
+                 ~long, ~lat, icon = parkIcon
+                 ,popup = paste("*Address:",preference()$address,"<br>"), group = "Parks") %>%
+      addMarkers(data = preference1(),
+                 ~long, ~lat, icon = riverIcon
+                 ,popup = paste("*Address:",preference1()$address,"<br>"), group = "Rivers & Pools"
+      ) %>%
+      addMarkers(data = preference2(),
+                 ~long, ~lat, icon = dogIcon
+                 ,popup = paste("*Address:",preference2()$address,"<br>"), group = "Dogs"
+      ) %>%
+      addMarkers(data = preference3(),
+                 ~long, ~lat, icon = waterIcon
+                 ,popup = paste("*Address:",preference3()$address,"<br>"), group = "Water Fountains"
+      ) %>%
+      addLayersControl(
+        position = "topright",
+        overlayGroups = c("Parks","Rivers & Pools","Dogs","Water Fountains"),
+        options = layersControlOptions(collapsed = FALSE)
+      ) 
   })
   
-  observeEvent(input$park, {
-    proxy <- leafletProxy("map2")
-    if (input$park ==TRUE) proxy %>% showGroup("parkers")
-    else proxy %>% hideGroup("parkers")
-  })
+  # observe({
+  #   proxy=
+  #     leafletProxy("map2",data = preference1())
+  #   proxy %>% 
+  #     # clearMarkers() %>%
+  #     addMarkers(#data = preference1(), 
+  #       ~long, ~lat, icon = riverIcon
+  #       ,popup = paste("*Address:",preference1()$address,"<br>"), group = "Rivers & Pools"
+  #     ) %>% 
+  #     addLayersControl(
+  #      
+  #       overlayGroups = c("Rivers & Pools"),
+  #       options = layersControlOptions(collapsed = FALSE)
+  #     )
+  # })
   
-  observe({
-    proxy=
-      leafletProxy("map2",data = preference1())
-    proxy %>% 
-      # clearMarkers() %>%
-      addMarkers(#data = preference1(), 
-        ~long, ~lat, icon = riverIcon
-        ,popup = paste("*Address:",preference1()$address,"<br>")
-      )
-  })
-  
-  observe({
-    proxy=
-      leafletProxy("map2",data = preference2())
-    proxy %>%
-      addMarkers(#data = preference2(),
-        ~long, ~lat,icon = dogIcon
-        ,popup = paste("*Address:",preference2()$address,"<br>")
-      )
-    
-  })
+  # observe({
+  #   proxy=
+  #     leafletProxy("map2",data = preference2())
+  #   proxy %>%
+  #     addMarkers(#data = preference2(),
+  #       ~long, ~lat,icon = dogIcon
+  #       ,popup = paste("*Address:",preference2()$address,"<br>")
+  #     )
+  #   
+  # })
   
   
-  observe({
-    proxy =
-      leafletProxy("map2",data = preference3()) 
-    proxy %>% 
-      # clearMarkers() %>%
-      addMarkers(#data = preference2(),
-        ~long, ~lat, icon = waterIcon
-        ,popup = paste("*Address:",preference3()$address,"<br>", layerId = "w")
-      )
-    
-  })
-  
+  # observe({
+  #   proxy =
+  #     leafletProxy("map2",data = preference3()) 
+  #   proxy %>% 
+  #     # clearMarkers() %>%
+  #     addMarkers(#data = preference2(),
+  #       ~long, ~lat, icon = waterIcon
+  #       ,popup = paste("*Address:",preference3()$address,"<br>", layerId = "w")
+  #     )
+  #   
+  # })
   
   
   
